5988d490db4e13f8f9b75212bd2b17789035e9f7
Dangield <dan.gield@gmail.com>
2021-07-12 14:43:43 +0200

    Last commit for old js version

--- dbObjects.js
+++ dbObjects.js
@@ -125,4 +125,150 @@ Users.prototype.getDuelCards = function() {
     });
 };
 
+Users.prototype.getAvailableDuelCards = function() {
+    return DuelCards.findAll({
+        where: {user_id: this.user_id, posX: 0, posY: 0},
+        include: ['card'],
+    });
+};
+
+Users.prototype.playCard = async function(duel,card,r,c) {
+    // card in hand
+    const usercard = await DuelCards.findOne({
+        where: {user_id: this.user_id, card_id: card.id, posX: 0, posY: 0},
+    })
+    if(!usercard) return false;
+
+    // place taken
+    const placeTaken1 = await DuelCards.findOne({
+        where: {user_id: duel.player1_id, posX: r, posY: c},
+    })
+    if(placeTaken1) return false;
+
+    const placeTaken2 = await DuelCards.findOne({
+        where: {user_id: duel.player2_id, posX: r, posY: c},
+    })
+    if(placeTaken2) return false;
+
+    // user had card in this row/column
+    const row = await DuelCards.findOne({
+        where: {user_id: this.user_id, posX: r},
+    })
+    if(row) return false;
+
+    const col = await DuelCards.findOne({
+        where: {user_id: this.user_id, posY: c},
+    })
+    if(col) return false;
+
+    usercard.posX = r;
+    usercard.posY = c;
+    return usercard.save();
+}
+
+Users.prototype.removeCardsFromDuel = async function() {
+    cards = await DuelCards.findAll({
+        where: {user_id: this.user_id},
+    });
+    for(i in cards) {
+        cards[i].destroy();
+    }
+    return false;
+}
+
+Duels.prototype.calculatePoints = async function() {
+    p1 = 0;
+    p2 = 0;
+    // body row
+    card1 = await DuelCards.findOne({
+        where: {user_id: this.player1_id, posX: 1},
+        include: ['card'],
+    })
+    card2 = await DuelCards.findOne({
+        where: {user_id: this.player2_id, posX: 1},
+        include: ['card'],
+    })
+    if (card1.card.body > card2.card.body) p1++;
+    else if (card1.card.body < card2.card.body) p2++;
+    // mind row
+    card1 = await DuelCards.findOne({
+        where: {user_id: this.player1_id, posX: 2},
+        include: ['card'],
+    })
+    card2 = await DuelCards.findOne({
+        where: {user_id: this.player2_id, posX: 2},
+        include: ['card'],
+    })
+    if (card1.card.mind > card2.card.mind) p1++;
+    else if (card1.card.mind < card2.card.mind) p2++;
+    // flair row
+    card1 = await DuelCards.findOne({
+        where: {user_id: this.player1_id, posX: 3},
+        include: ['card'],
+    })
+    card2 = await DuelCards.findOne({
+        where: {user_id: this.player2_id, posX: 3},
+        include: ['card'],
+    })
+    if (card1.card.flair > card2.card.flair) p1++;
+    else if (card1.card.flair < card2.card.flair) p2++;
+    // charm row
+    card1 = await DuelCards.findOne({
+        where: {user_id: this.player1_id, posX: 4},
+        include: ['card'],
+    })
+    card2 = await DuelCards.findOne({
+        where: {user_id: this.player2_id, posX: 4},
+        include: ['card'],
+    })
+    if (card1.card.charm > card2.card.charm) p1++;
+    else if (card1.card.charm < card2.card.charm) p2++;
+    // body col
+    card1 = await DuelCards.findOne({
+        where: {user_id: this.player1_id, posY: 1},
+        include: ['card'],
+    })
+    card2 = await DuelCards.findOne({
+        where: {user_id: this.player2_id, posY: 1},
+        include: ['card'],
+    })
+    if (card1.card.body > card2.card.body) p1++;
+    else if (card1.card.body < card2.card.body) p2++;
+    // mind col
+    card1 = await DuelCards.findOne({
+        where: {user_id: this.player1_id, posY: 2},
+        include: ['card'],
+    })
+    card2 = await DuelCards.findOne({
+        where: {user_id: this.player2_id, posY: 2},
+        include: ['card'],
+    })
+    if (card1.card.mind > card2.card.mind) p1++;
+    else if (card1.card.mind < card2.card.mind) p2++;
+    // flair col
+    card1 = await DuelCards.findOne({
+        where: {user_id: this.player1_id, posY: 3},
+        include: ['card'],
+    })
+    card2 = await DuelCards.findOne({
+        where: {user_id: this.player2_id, posY: 3},
+        include: ['card'],
+    })
+    if (card1.card.flair > card2.card.flair) p1++;
+    else if (card1.card.flair < card2.card.flair) p2++;
+    // charm col
+    card1 = await DuelCards.findOne({
+        where: {user_id: this.player1_id, posY: 4},
+        include: ['card'],
+    })
+    card2 = await DuelCards.findOne({
+        where: {user_id: this.player2_id, posY: 4},
+        include: ['card'],
+    })
+    if (card1.card.charm > card2.card.charm) p1++;
+    else if (card1.card.charm < card2.card.charm) p2++;
+
+    return [p1,p2];
+}
+
 module.exports = { Users, CardCompendium, UserCards, Duels, DuelCards };
