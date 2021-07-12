174b1a529cd8c4f030d417acbbeef2a873bfd873
Dangield <dan.gield@gmail.com>
2021-07-13 00:59:53 +0200

    Players can now create their own characters, with integer attributes and functions that use them and can be rolled.

--- commands/rpg_commands.py
+++ commands/rpg_commands.py
@@ -2,6 +2,7 @@ import discord
 import random
 from discord.ext import commands
 import re
+import yaml
 
 # RPG commands class
 class RPGCommands(commands.Cog, name = 'RPG commands'):
@@ -75,3 +76,136 @@ class RPGCommands(commands.Cog, name = 'RPG commands'):
 			rolls.reverse()
 			response += '[' + ','.join([str(roll) for roll in rolls]) + ',~~' + str(minimum) + '~~]: ' + str(sum(rolls)) + '\n'
 		await ctx.send(response[0:-1])
+
+	@commands.group(name = 'character', aliases = ['char'], brief = 'Commands for player characters.')
+	async def character(self, ctx):
+		if ctx.invoked_subcommand is None:
+			await ctx.send('Invalid subcommand passed...')
+
+	@character.command(name = 'create', aliases = ['c'], brief = 'Create new character.')
+	async def create_new_character(self, ctx, name):
+		data = self.get_player_data_from_file(ctx.author.id)
+		if name in list(data['characters'].keys()):
+			await ctx.send(ctx.author.mention + ', you already have a character with the name: ' + name + '.')
+			return
+		data['characters'][name] = {'attributes': {}, 'functions': {}}
+		if data['current_character'] is None:
+			data['current_character'] = name
+		self.write_player_data_to_file(ctx.author.id, data)
+		await ctx.send('You have created a new character: ' + name)
+
+	@character.command(name = 'list', aliases = ['l'], brief = 'List your characters.')
+	async def list_player_characters(self, ctx):
+		data = self.get_player_data_from_file(ctx.author.id)
+		if not data['characters']:
+			await ctx.send(ctx.author.mention + ', you have no characters.')
+		else:
+			await ctx.send(ctx.author.mention + ', you have the following characters: ' + ', '.join(list(data['characters'].keys())))
+
+	@character.command(name = 'current', aliases = ['curr'], brief = 'Show your current character.')
+	async def show_current_character(self, ctx):
+		data = self.get_player_data_from_file(ctx.author.id)
+		if data['current_character'] is None:
+			await ctx.send(ctx.author.mention + ', you have no current character.')
+		else:
+			e = discord.Embed(title = data['current_character'])
+			ch = data['characters'][data['current_character']]
+			e.add_field(name = 'Attributes', value = 'None' if not ch['attributes'] else '\n'.join([k + ': ' + str(ch['attributes'][k]) for k in list(ch['attributes'].keys())]))
+			e.add_field(name = 'Functions', value = 'None' if not ch['functions'] else '\n'.join(list(ch['functions'].keys())))
+			await ctx.send(embed = e)
+
+	@character.command(name = 'change', aliases = ['ch'], brief = 'Choose currently used character.')
+	async def choose_current_character(self, ctx, name):
+		data = self.get_player_data_from_file(ctx.author.id)
+		if not data['characters']:
+			await ctx.send(ctx.author.mention + ', you have no characters.')
+		if name in list(data['characters'].keys()):
+			data['current_character'] = name
+			self.write_player_data_to_file(ctx.author.id, data)
+		else:
+			await ctx.send(ctx.author.mention + ', you don\'t have character with such name.')
+
+	@character.group(name = 'attribute', aliases = ['att', 'a'], brief = 'Commands for player character\'s attribute')
+	async def attribute(self, ctx):
+		if ctx.invoked_subcommand is None:
+			await ctx.send('Invalid subcommand passed...')
+
+	@attribute.command(name = 'add', aliases = ['a'], brief = 'Add new attribute or change existing.')
+	async def add_new_attribute(self, ctx, name, value = 0):
+		data = self.get_player_data_from_file(ctx.author.id)
+		data['characters'][data['current_character']]['attributes'][name] = int(value)
+		self.write_player_data_to_file(ctx.author.id, data)
+
+	@attribute.command(name = 'remove', aliases = ['r'], brief = 'Remove attribute of selected name.')
+	async def remove_attribute(self, ctx, name):
+		data = self.get_player_data_from_file(ctx.author.id)
+		data['characters'][data['current_character']]['attributes'].pop(name, None)
+		self.write_player_data_to_file(ctx.author.id, data)
+
+	@attribute.command(name = 'list', aliases = ['l'], brief = 'List all attributes.')
+	async def list_attributes(self, ctx):
+		data = self.get_player_data_from_file(ctx.author.id)
+		if data['current_character'] is None:
+			await ctx.send(ctx.author.mention + ', you have no current character.')
+		else:
+			e = discord.Embed(title = data['current_character'])
+			ch = data['characters'][data['current_character']]
+			e.add_field(name = 'Attributes', value = 'None' if not ch['attributes'] else '\n'.join([k + ': ' + str(ch['attributes'][k]) for k in list(ch['attributes'].keys())]))
+			await ctx.send(embed = e)
+
+	@character.group(name = 'function', aliases = ['f', 'fun'], brief = 'Commands for player character\'s functions.')
+	async def function(self, ctx):
+		if ctx.invoked_subcommand is None:
+			await ctx.send('Invalid subcommand passed...')
+
+	@function.command(name = 'add', aliases = ['a'], brief = 'Add new function.')
+	async def add_new_function(self, ctx, name, rule):
+		data = self.get_player_data_from_file(ctx.author.id)
+		data['characters'][data['current_character']]['functions'][name] = rule
+		self.write_player_data_to_file(ctx.author.id, data)
+
+	@function.command(name = 'remove', aliases = ['r'], brief = 'Remove existing function.')
+	async def remove_function(self, ctx, name):
+		data = self.get_player_data_from_file(ctx.author.id)
+		data['characters'][data['current_character']]['functions'].pop(name, None)
+		self.write_player_data_to_file(ctx.author.id, data)
+
+	@function.command(name = 'list', aliases = ['l'], brief = 'List all functions.')
+	async def list_functions(self, ctx):
+		data = self.get_player_data_from_file(ctx.author.id)
+		if data['current_character'] is None:
+			await ctx.send(ctx.author.mention + ', you have no current character.')
+		else:
+			e = discord.Embed(title = data['current_character'])
+			ch = data['characters'][data['current_character']]
+			e.add_field(name = 'Attributes', value = 'None' if not ch['functions'] else '\n'.join([k + ': ' + str(ch['functions'][k]) for k in list(ch['functions'].keys())]))
+			await ctx.send(embed = e)
+
+	@function.command(name = 'execute', aliases = ['e'], brief = 'Execute the chosen function.')
+	async def execute_function(self, ctx, fun):
+		data = self.get_player_data_from_file(ctx.author.id)
+		ch = data['characters'][data['current_character']]
+		try:
+			response, final_sum = self.perform_roll(self.change_attributes_in_funtion_to_values(ch['functions'][fun], ch['attributes']))
+			await ctx.send(ctx.author.mention + ' ' + fun + ': ' + ' = '.join([response, str(final_sum)]))
+		except KeyError:
+			await ctx.send('No such function for your character.')
+
+	def change_attributes_in_funtion_to_values(self, rule, attributes):
+		arguments = re.split('\\+|\\-|x', rule)
+		operations = list(filter(lambda a: a in ['+', '-', 'x'], rule))
+		for i in range(len(arguments)):
+			if arguments[i] in list(attributes.keys()):
+				arguments[i] = str(attributes[arguments[i]])
+		return arguments[0] + ''.join([operations[i] + arguments[i+1] for i in range(len(operations))])
+
+	def get_player_data_from_file(self, id):
+		try:
+			with open('yaml/' + str(id) + '.yaml', 'r') as f:
+				return yaml.load(f, Loader=yaml.SafeLoader)
+		except FileNotFoundError:
+			return {'current_character': None, 'characters': {}}
+
+	def write_player_data_to_file(self, id, data):
+		with open('yaml/' + str(id) + '.yaml', 'w') as f:
+			yaml.dump(data, f)
